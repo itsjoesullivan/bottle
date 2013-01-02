55034adc34a21129b546e1ea29a04c2edc08a408
Joe Sullivan <joe@clearspring.com>
2013-01-02 13:47:07 -0500

    bottle.js

---
+++ bottle.js
@@ -0,0 +1,17 @@
+var bottle = function(fn,pct) {
+	var lastTime = new Date().getTime(),
+		startTime,
+		res,
+		whenLast,
+		lastDuration = 0;
+	return function() {
+		startTime = new Date().getTime();
+		if(lastDuration/pct < startTime - whenLast) {
+			startTime = new Date().getTime();
+			res = fn(); 
+			whenLast = new Date().getTime();
+			lastDuration = whenLast - startTime;
+			return res;
+		}       
+	};
+};   
