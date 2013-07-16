735241cd667be0351affc93bd7c96bf15d5dd23d
itsjoesullivan <itsjoesullivan@gmail.com>
2013-07-16 10:43:21 -0400

    New specs. Variable naming.

--- bottle.js
+++ bottle.js
@@ -1,17 +1,24 @@
+/** Limits time spent processing a given function 
+ * 
+ * @param {Function} fn The subject function
+ * @param {Number} pct The upper bound of CPU use allowed the function
+ * @return {Function} The bottled function
+ *
+ * */
 var bottle = function(fn,pct) {
 	var lastTime = new Date().getTime(),
 		startTime,
-		res,
+		bottledFn,
 		whenLast,
 		lastDuration = 0;
 	return function() {
 		startTime = new Date().getTime();
 		if(!whenLast || lastDuration/pct < startTime - whenLast) {
 			startTime = new Date().getTime();
-			res = fn(); 
+			bottledFn = fn.apply(fn,arguments)
 			whenLast = new Date().getTime();
 			lastDuration = whenLast - startTime;
-			return res;
+			return bottledFn;
 		}       
 	};
 };   
--- bottle.spec.js
+++ bottle.spec.js
@@ -1,9 +1,15 @@
 require('./bottle');
 var bottleText = require('fs').readFileSync('./bottle.js', 'binary');
+
+// node
+if(typeof window === 'undefined') {
+	expect = require('chai').expect;
+
+}
 eval(bottleText);
 describe('bottle', function() {
 	it('exists', function() {
-		expect(typeof bottle).toEqual('function');
+		(typeof bottle).should.equal('function');
 	});	
 
 	it('returns a function', function() {
@@ -11,7 +17,20 @@ describe('bottle', function() {
 			return 'hello';
 		};
 		var bottledFunction = bottle(fn,1);
-		expect(typeof bottle).toEqual('function')
+		(typeof bottle).should.equal('function')
+	});
+	it('accepts arguments', function() {
+		var fn = function(arg) {
+			return arg;
+		};
+		var bottled = bottle(fn,1);
+		bottled('asdf').should.equal('asdf');
+	});
+	it('retains a bound context', function() {
+		var fn = function(arg) {
+			return this;
+		}.bind('asdf');
+		bottle(fn,1)().should.equal('asdf');
 	});
 	it('runs a function once, definitely.', function() {
 		var fn = function() {
@@ -19,23 +38,29 @@ describe('bottle', function() {
 		};
 		var bottledFunction = bottle(fn,1);
 		var res = bottledFunction();
-		expect(res).toEqual('hello');
+		(res).should.equal('hello');
 	});
-	it('doesnt run really really slow functions too much', function() {
+	it('doesnt run really really slow functions too much', function(done) {
+		// Define a heavy fn
 		var fn = function() {
 			var ct = 0;
-			while(ct++ < Math.pow(2,21)){
+			while(ct++ < Math.pow(2,18)){
 			}
 			return 'hello';
 		}
+
+		// Time it
 		var startTime = new Date().getTime();
 		fn();
 		var took = new Date().getTime() - startTime;
-		var ct = 11;
 		var performed = 0;
 		bottledFn = bottle(fn,.75);
+
+		
+		var ct = 11;
 		while(ct--) {
 			(function() {
+				// Cast inside closure
 				var count = parseInt(''+ ct);
 				setTimeout(function() {
 					var res = bottledFn();
@@ -43,12 +68,13 @@ describe('bottle', function() {
 						performed++;
 					}
 					if(count === 10) {
-						asyncSpecDone();
+						console.log(performed / 10);
+						
+						(performed).should.be.below(11);
+						done();
 					}
-				},(took/2)*ct);
+				},(took/1)*ct);
 			})();
 		}
-		asyncSpecWait();
-		expect(performed).toBeLessThan(11);
 	});
 });
